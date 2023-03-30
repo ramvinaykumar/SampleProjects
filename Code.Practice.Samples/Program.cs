9a384fbc3493319b4f7da8171c0524f6df68dabc
Ram Vinay Kumar <ramkumarkumar@hotmail.com>
2023-03-30 10:12:11 +0530

    Learning about DateTimeOffSet

--- Code.Practice.Samples/Program.cs
+++ Code.Practice.Samples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Code.Practice.Samples.Accounting;
 using Code.Practice.Samples.BankAccountNumberValidation;
 using Code.Practice.Samples.Basics;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -139,6 +140,46 @@ namespace Code.Practice.Samples
 
             //BankAccountValidationService validationService = new BankAccountValidationService();
             //validationService.A55ValidatePaymentInputAsync(inputRequestDto);
+
+
+            // Get current DateTime. It can be any DateTime object in your code.
+            DateTime aDate = DateTime.Now;
+
+            // Format Datetime in different formats and display them
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy"));
+            Console.WriteLine(aDate.ToString("dd MMM yyyy hh:mm tt"));
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy HH:mm"));
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy hh:mm tt"));
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy H:mm"));
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy h:mm tt"));
+            Console.WriteLine(aDate.ToString("MM/dd/yyyy HH:mm:ss"));
+            Console.WriteLine(aDate.ToString("MMMM dd"));
+            Console.WriteLine(aDate.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK"));
+            Console.WriteLine(aDate.ToString("ddd, dd MMM yyy HH’:’mm’:’ss ‘GMT’"));
+            Console.WriteLine(aDate.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss"));
+            Console.WriteLine(aDate.ToString("HH:mm"));
+            Console.WriteLine(aDate.ToString("hh:mm tt"));
+            Console.WriteLine(aDate.ToString("H:mm"));
+            Console.WriteLine(aDate.ToString("h:mm tt"));
+            Console.WriteLine(aDate.ToString("HH:mm:ss"));
+            Console.WriteLine(aDate.ToString("yyyy MMMM"));
+
+            // Decimal Issue
+            var stringValue = string.Empty;
+            var decimalValue = string.IsNullOrEmpty(stringValue) ? 0 : Convert.ToDecimal(stringValue);
+            DateTimeOffset thisDate;
+            thisDate = DateTimeOffset.UtcNow;
+            Console.WriteLine("DateTimeOffset into string format " + thisDate.ToString("dd MMM yyyy"));
+
+            DateTimeOffset PassDate = DateTimeOffset.Now;
+            var dateFormat = "dd MMM yyyy";
+            var x = PassDate.ToString(dateFormat);
+            Console.WriteLine("DateTimeOffset into string format " + x);
 
             Console.ReadLine();
         }
